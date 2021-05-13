6b894bbb782599bb0b979138c79a066eb151de91
Rayago <raymundothiago@hotmail.com>
2021-05-13 12:35:09 -0300

    :art: Refatora retorno do cálculo da distância entre cidades

--- src/main/java/com/github/rayago/citiesapi/distances/DistanceResource.java
+++ src/main/java/com/github/rayago/citiesapi/distances/DistanceResource.java
@@ -3,6 +3,7 @@ package com.github.rayago.citiesapi.distances;
 import com.github.rayago.citiesapi.distances.service.DistanceService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,17 +21,17 @@ public class DistanceResource {
     }
 
     @GetMapping("/by-points")
-    public Double byPoints(@RequestParam(name = "from") final Long city1,
-                           @RequestParam(name = "to") final Long city2) {
+    public ResponseEntity byPoints(@RequestParam(name = "from") final Long city1,
+                                   @RequestParam(name = "to") final Long city2) {
         log.info("byPoints");
-        return service.distanceByPointsInMiles(city1, city2);
+        return ResponseEntity.ok().body(service.distanceByPointsInMiles(city1, city2));
     }
 
     @GetMapping("/by-cube")
-    public Double byCube(@RequestParam(name = "from") final Long city1,
+    public ResponseEntity byCube(@RequestParam(name = "from") final Long city1,
                          @RequestParam(name = "to") final Long city2) {
         log.info("byCube");
-        return service.distanceByCubeInMeters(city1, city2);
+        return ResponseEntity.ok().body(service.distanceByCubeInMeters(city1, city2));
     }
 
     /*
