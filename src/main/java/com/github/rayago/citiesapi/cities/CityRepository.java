6f43e018e7f5023315648ca9a54b9380b3a4f17e
Rayago <raymundothiago@hotmail.com>
2021-05-13 10:42:40 -0300

    :rocket: Adiciona Consulta de cidades e a latitude e longitude de cada uma

---
+++ src/main/java/com/github/rayago/citiesapi/cities/CityRepository.java
@@ -0,0 +1,7 @@
+package com.github.rayago.citiesapi.cities;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityRepository extends JpaRepository<City, Long> {
+
+}
